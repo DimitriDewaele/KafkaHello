301cd510901bcfbd534ba38509fcc403565867e1
dimitri.dewaele <dimitri.dewaele@wolterskluwer.com>
2019-11-29 15:07:11 +0100

    Kafka producer

--- src/main/java/be/softwarelab/kafka/KafkaProducer.java
+++ src/main/java/be/softwarelab/kafka/KafkaProducer.java
@@ -1,6 +1,13 @@
 package be.softwarelab.kafka;
 
+import com.sun.istack.internal.NotNull;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
 
 @Slf4j
 public class KafkaProducer {
@@ -24,4 +31,20 @@ public class KafkaProducer {
         }
         log.info("==== Produce {} messages.", messageCount);
     }
+
+    /**
+     * Create a Kafka producer
+     *
+     * @return kafkaProducer, never null
+     */
+    @NotNull
+    private static Producer<Long, String> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return new org.apache.kafka.clients.producer.KafkaProducer<Long, String>(props);
+    }
+
 }
